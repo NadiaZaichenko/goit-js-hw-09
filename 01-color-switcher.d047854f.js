08b4a124b5b68b1750adc0af99c92e8428152030
NadiaZaichenko <114259833+NadiaZaichenko@users.noreply.github.com>
2023-03-12 13:56:39 +0000

    Deploying to gh-pages from @ NadiaZaichenko/goit-js-hw-09@ae7410373dfd50a04520bcd0fa7fe339591fd71e 🚀

---
+++ 01-color-switcher.d047854f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=null;function a(e){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}n.disabled=!0,e.addEventListener("click",(function(t){t.preventDefault(),a(t),o=setInterval((function(){return a(t)}),1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1,n.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.d047854f.js.map
